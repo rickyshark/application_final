a5baf3a493c3858ad4fe1183093c9b343c9f79ca
rickylasalsa <monteroricky4@gmail.com>
2021-08-19 19:57:06 -0400

    Modificaciones
    Cambio en el archivo asignaturas

--- lib/MenuLateral/drawer.dart
+++ lib/MenuLateral/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_final/Pages/asignaturasP.dart';
+import 'package:flutter_application_final/Pages/calificaciones.dart';
 import 'package:line_icons/line_icon.dart';
 import 'package:line_icons/line_icons.dart';
 
